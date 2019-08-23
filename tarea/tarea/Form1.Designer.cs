ce3cee5fd2709b34eb778628fb0f499af0e6d298
Diego Armando Alvarez Figueroa <dalvarez12@ucol.mx>
2019-08-23 18:40:40 -0500

    Nombres

--- tarea/tarea/Form1.Designer.cs
+++ tarea/tarea/Form1.Designer.cs
@@ -28,77 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.LbName = new System.Windows.Forms.Label();
+            this.LbPassword = new System.Windows.Forms.Label();
+            this.BtLogin = new System.Windows.Forms.Button();
+            this.BtExit = new System.Windows.Forms.Button();
+            this.TbName = new System.Windows.Forms.TextBox();
+            this.TbPassword = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // label1
+            // LbName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(88, 69);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.LbName.AutoSize = true;
+            this.LbName.Location = new System.Drawing.Point(88, 69);
+            this.LbName.Name = "LbName";
+            this.LbName.Size = new System.Drawing.Size(43, 13);
+            this.LbName.TabIndex = 0;
+            this.LbName.Text = "Usuario";
             // 
-            // label2
+            // LbPassword
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(88, 135);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.LbPassword.AutoSize = true;
+            this.LbPassword.Location = new System.Drawing.Point(88, 135);
+            this.LbPassword.Name = "LbPassword";
+            this.LbPassword.Size = new System.Drawing.Size(61, 13);
+            this.LbPassword.TabIndex = 1;
+            this.LbPassword.Text = "Contraseña";
             // 
-            // button1
+            // BtLogin
             // 
-            this.button1.Location = new System.Drawing.Point(542, 261);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 42);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BtLogin.Location = new System.Drawing.Point(542, 261);
+            this.BtLogin.Name = "BtLogin";
+            this.BtLogin.Size = new System.Drawing.Size(132, 42);
+            this.BtLogin.TabIndex = 2;
+            this.BtLogin.Text = "Iniciar sesión";
+            this.BtLogin.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // BtExit
             // 
-            this.button2.Location = new System.Drawing.Point(153, 258);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(153, 45);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.BtExit.Location = new System.Drawing.Point(153, 258);
+            this.BtExit.Name = "BtExit";
+            this.BtExit.Size = new System.Drawing.Size(153, 45);
+            this.BtExit.TabIndex = 3;
+            this.BtExit.Text = "Salir";
+            this.BtExit.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // TbName
             // 
-            this.textBox1.Location = new System.Drawing.Point(392, 46);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(192, 20);
-            this.textBox1.TabIndex = 4;
+            this.TbName.Location = new System.Drawing.Point(392, 46);
+            this.TbName.Name = "TbName";
+            this.TbName.Size = new System.Drawing.Size(192, 20);
+            this.TbName.TabIndex = 4;
             // 
-            // textBox2
+            // TbPassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(392, 117);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(188, 20);
-            this.textBox2.TabIndex = 5;
+            this.TbPassword.Location = new System.Drawing.Point(392, 117);
+            this.TbPassword.Name = "TbPassword";
+            this.TbPassword.Size = new System.Drawing.Size(188, 20);
+            this.TbPassword.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TbPassword);
+            this.Controls.Add(this.TbName);
+            this.Controls.Add(this.BtExit);
+            this.Controls.Add(this.BtLogin);
+            this.Controls.Add(this.LbPassword);
+            this.Controls.Add(this.LbName);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Avengers Assamble - Iniciar sesión";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,12 +106,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label LbName;
+        private System.Windows.Forms.Label LbPassword;
+        private System.Windows.Forms.Button BtLogin;
+        private System.Windows.Forms.Button BtExit;
+        private System.Windows.Forms.TextBox TbName;
+        private System.Windows.Forms.TextBox TbPassword;
     }
 }
 
